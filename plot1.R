244379ae1b88005919623064c4d4be3b212a9a3d
VJ <vj@vjkapur.com>
2014-05-11 16:48:29 -0400

    initial commit; finished plot1

---
+++ plot1.R
@@ -0,0 +1,21 @@
+##handle file matters
+fileName <- "household_power_consumption.txt"
+
+if(!file.exists(fileName)) {
+	zipFile <- "zip1.zip"
+	fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+	download.file(fileUrl, destfile=zipFile, method="curl")
+	unzip(zipFile)
+	file.remove(zipFile)
+}
+
+##read in data, subset by date
+data <- read.table("household_power_consumption.txt", na.strings = "?", sep = ";", header = TRUE)
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+subdata <- subset(data, Date == "2007-02-01" | Date == "2007-02-02")
+rm(data)
+
+##output to PNG
+png(file='plot1.png')
+hist(subdata$Global_active_power,ylim=c(0,1200), breaks = "sturges",xlab='Global Active Power (kilowatts)',main='Global Active Power',col="red")
+dev.off()
